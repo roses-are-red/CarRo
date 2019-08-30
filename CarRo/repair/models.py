c2959511a227e199620a55cbd07b27b0f9ea71a0
roses-are-red <praghya.anilkumar@gmail.com>
2019-08-30 11:16:29 -0400

    fixed update on repair order and postman collections.

--- CarRo/repair/models.py
+++ CarRo/repair/models.py
@@ -22,7 +22,18 @@ states = ("ORDERED", "REPAIRING", "REPAIRED", "REVIEWING", "DONE")
 class RepairOrder(Document):
     id = SequenceField(primary_key=True)
     date = DateTimeField(default=timezone.now)
-    vehicle = EmbeddedDocumentField(Vehicle)
-    customer = EmbeddedDocumentField(Customer)
-    status = StringField(choices=states, default="ORDERED")
-    complaint = StringField(max_length=300)
+    vehicle = EmbeddedDocumentField(Vehicle, required=True)
+    customer = EmbeddedDocumentField(Customer, required=True)
+    status = StringField(choices=states, default="ORDERED", required=True)
+    complaint = StringField(max_length=300, required=True)
+
+    def is_valid_transition(self, to_state):
+        if self.status == "ORDERED":
+            return to_state == "REPAIRING"
+        if self.status == "REPAIRING":
+            return to_state == "REPAIRED"
+        if self.status == "REPAIRED":
+            return to_state == "REVIEWING"
+        if self.status == "REVIEWING":
+            return to_state in ["REPAIRING", "DONE"]
+        return False
--- CarRo/repair/serializers.py
+++ CarRo/repair/serializers.py
@@ -26,3 +26,10 @@ class RepairOrderSerializer(DocumentSerializer):
     def create(self, validated_data):
         validated_data.update({"status": "ORDERED"})
         return super(RepairOrderSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data.pop('date')
+        to_state = validated_data.pop('status')
+        if instance.is_valid_transition(to_state):
+            instance.status = to_state
+        return super(RepairOrderSerializer, self).update(instance, validated_data)
